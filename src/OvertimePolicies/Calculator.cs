1ff3880e00f9df46de4337eeff052459b5f99125
s.kavianmehr <s.kavianmehr@S-KAVIANI>
2023-01-29 15:14:47 +0330

    Add project files.

---
+++ src/OvertimePolicies/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace OvertimePolicies;
+public class Calculator
+{
+    public double CalcurlatorA()
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalcurlatorB()
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalcurlatorC()
+    {
+        throw new NotImplementedException();
+    }
+}
